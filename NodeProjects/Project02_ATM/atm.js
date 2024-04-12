0fbc08e235a6d0bf8c03f81469c4e85a3ada6b94
Tayyab khalid <tayyabkhalid993@gmial.com>
2024-04-12 11:52:10 +0500

    Completing the project of ATM

--- NodeProjects/Project02_ATM/atm.js
+++ NodeProjects/Project02_ATM/atm.js
@@ -1,6 +1,8 @@
 import inquirer from 'inquirer';
-let username = ['Tayyab'];
-let userpass = ['123'];
+import chalk from 'chalk';
+let userName = ['Tayyab'];
+let userPass = ['123'];
+let balance = 50000;
 const atm = await inquirer.prompt([
     {
         type: 'list',
@@ -9,30 +11,64 @@ const atm = await inquirer.prompt([
         choices: ['Sign in', 'Sign up']
     }
 ]);
+if (atm.login == "Sign up") {
+    console.log(chalk.yellowBright("Sorry! this feature is not available"));
+}
 if (atm.login === 'Sign in') {
     const signIn = await inquirer.prompt([
         {
             type: 'string',
             name: 'userName',
-            message: 'Enter user name'
+            message: chalk.green('Enter user name : ')
         },
         {
             type: 'password',
             name: 'pass',
-            message: 'enter your password : '
+            message: chalk.green('enter your password :')
         }
     ]);
-    if (signIn.userName == username[0] && (signIn.pass == userpass[0])) {
-        const moreOption = await inquirer.prompt([
+    if (signIn.userName == userName[0] && (signIn.pass == userPass[0])) {
+        const operations = await inquirer.prompt([
             {
                 type: 'list',
                 name: 'options',
-                message: "Select any of them",
-                choices: ['Fast cash', 'Cash withdraw', 'cash transfer']
+                message: "Select any of them : ",
+                choices: ['Fast cash', 'Cash withdraw', 'Check Balance']
             }
         ]);
+        if (operations.options == "Check Balance") {
+            console.log(chalk.green(`your balance is ${balance}`));
+        }
+        if (operations.options == 'Cash withdraw') {
+            const withdraw = await inquirer.prompt([
+                {
+                    name: "withdrawAmount",
+                    type: "number",
+                    message: "Enter withdraw amount : "
+                }
+            ]);
+            if (withdraw.withdrawAmount <= balance) {
+                console.log(chalk.greenBright("Your remaining balance is ", +balance - withdraw.withdrawAmount));
+            }
+            else {
+                console.log(chalk.red("You can't withdraw more than your balance."));
+            }
+        }
+        if (operations.options == "Fast cash") {
+            const fastCash = await inquirer.prompt([
+                {
+                    name: "fastAmount",
+                    type: "list",
+                    message: "Choose any amount : ",
+                    choices: [500, 1000, 5000, 10000]
+                }
+            ]);
+            if (fastCash.fastAmount <= balance) {
+                console.log(chalk.green("your remaining balance is ", +balance - fastCash.fastAmount));
+            }
+        }
     }
     else {
-        console.log("Please enter correct user name or Password");
+        console.log(chalk.red("Please enter correct user name or Password"));
     }
 }
--- NodeProjects/Project02_ATM/atm.ts
+++ NodeProjects/Project02_ATM/atm.ts
@@ -1,7 +1,9 @@
 import inquirer from 'inquirer';
+import chalk from 'chalk';
 
-let username : string[] = ['Tayyab'];
-let userpass : string[] = ['123'];
+let userName : string[] = ['Tayyab'];
+let userPass : any[] = ['123'];
+let balance  : Number = 50000;
 
 const atm = await inquirer.prompt([
     {
@@ -9,37 +11,79 @@ const atm = await inquirer.prompt([
         name:'login',
         message:'Choose an option from given blow :',
         choices : ['Sign in', 'Sign up']   
-    }
+    }   
     
 ])
+
+if(atm.login == "Sign up"){
+    console.log(chalk.yellowBright("Sorry! this feature is not available"))
+}
 
 if(atm.login === 'Sign in'){
     const signIn = await inquirer.prompt([
         {
             type : 'string',
             name : 'userName',
-            message : 'Enter user name'
+            message :chalk.green( 'Enter user name : ')
         },
         {
             type : 'password',
             name : 'pass',
-            message : 'enter your password : '
+            message :chalk.green('enter your password :')
         }
     ])  
-    if(signIn.userName==username[0] && (signIn.pass == userpass[0])){
-        const moreOption = await inquirer.prompt([
+    
+    if(signIn.userName==userName[0] && (signIn.pass == userPass[0])){
+        const operations = await inquirer.prompt([
             {
                 type : 'list',
                 name : 'options',
-                message : "Select any of them",
-                choices : ['Fast cash','Cash withdraw','cash transfer']
+                message : "Select any of them : ",
+                choices : ['Fast cash','Cash withdraw','Check Balance']
             }
         ])
+
+        if(operations.options == "Check Balance"){
+
+            console.log(chalk.green(`your balance is ${balance}`));
+        }
+
+        if(operations.options == 'Cash withdraw'){
+
+            const withdraw = await inquirer.prompt([
+                {
+                    name : "withdrawAmount",
+                    type : "number",
+                    message : "Enter withdraw amount : "
+                }
+            ])
+            if(withdraw.withdrawAmount <= balance){
+                console.log(chalk.greenBright("Your remaining balance is ",+ balance - withdraw.withdrawAmount));
+            }
+            else{
+                console.log(chalk.red("You can't withdraw more than your balance."));
+
+            }
+        }
+
+        if(operations.options == "Fast cash"){
+            const fastCash = await inquirer.prompt([
+                {
+                    name : "fastAmount",
+                    type : "list",
+                    message : "Choose any amount : ",
+                    choices :[500,1000,5000,10000]
+                }
+            ])
+            if(fastCash.fastAmount <= balance ){
+
+                console.log(chalk.green("your remaining balance is ",+balance -fastCash.fastAmount));
+
+        }
+    }
     }
 
     else {
-        console.log("Please enter correct user name or Password")
+        console.log(chalk.red("Please enter correct user name or Password"))
     }
 }
-
-
