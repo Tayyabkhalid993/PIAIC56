df752cb698853fe04d25e3068e9ec91838f2c83f
Tayyab khalid <tayyabkhalid993@gmial.com>
2024-04-19 22:25:13 +0500

    adding welcome phrase

--- NodeProjects/Project05_word_counter/word_counter.js
+++ NodeProjects/Project05_word_counter/word_counter.js
@@ -1,11 +1,15 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from 'chalk';
+console.log(`\n\t>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<\n`);
+console.log(chalk.bold.yellowBright("Welcome to the word counter program by Tayyab Khalid\n"));
+console.log(`\t>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<\n`);
 const answer = await inquirer.prompt([
     {
         name: "sentence",
         type: "input",
-        message: chalk.bold.blue("Write any sentence for count the word.  ")
+        message: chalk.bold.bgBlue("Write any sentence for count the word.  "),
+        default: "I love typescript."
     }
 ]);
 // trim() is remove the white spaces from the start and end of the paragraph
--- NodeProjects/Project05_word_counter/word_counter.ts
+++ NodeProjects/Project05_word_counter/word_counter.ts
@@ -4,13 +4,17 @@ import inquirer from "inquirer";
 
 import chalk from 'chalk';
 
+console.log(`\n\t>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<\n`);
+console.log(chalk.bold.yellowBright("Welcome to the word counter program by Tayyab Khalid\n"));
+console.log(`\t>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<\n`);
 const answer : {
     sentence : string
 } = await inquirer.prompt([
     {
         name : "sentence",
         type : "input",
-        message : chalk.bold.blue("Write any sentence for count the word.  ")
+        message : chalk.bold.bgBlue("Write any sentence for count the word.  "),
+        default : "I love typescript."
     }
 ])
 
