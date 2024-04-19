8bc32218caafe65dcde99616e66454de1defcbf8
Tayyab khalid <tayyabkhalid993@gmial.com>
2024-04-19 18:30:26 +0500

    pushing project word counter

---
+++ NodeProjects/Project05_word_counter/word_counter.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+import chalk from 'chalk';
+
+const answer : {
+    sentence : string
+} = await inquirer.prompt([
+    {
+        name : "sentence",
+        type : "input",
+        message : chalk.bold.blue("Write any sentence for count the word.  ")
+    }
+])
+
+// trim() is remove the white spaces from the start and end of the paragraph
+//split() is remove the from the sentence.
+
+const word = answer.sentence.trim().split(" ");
+
+console.log(word);
+
