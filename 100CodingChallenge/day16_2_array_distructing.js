f670996aefe8b51660bf224702483665522edb6e
Tayyab khalid <tayyabkhalid993@gmial.com>
2024-07-15 22:02:59 +0500

    adding comments

--- 100CodingChallenge/day16_2_array_distructing.js
+++ 100CodingChallenge/day16_2_array_distructing.js
@@ -10,6 +10,7 @@ let laptops = [
     { make: "Apple", model: "MacBook Pro", year: 2020 },
     { make: "HP", model: "Spectre x360", year: 2021 }
 ];
+// Array Destructing 
 let [laptop1, laptop2] = laptops;
 console.log(laptop1);
 console.log(laptop2);
